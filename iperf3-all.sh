15c696049f12a627c41716b435080f2c47f25f63
pscnoc <pscnoc@ps-test.psc.edu>
2015-10-28 15:41:03 -0400

    added ping and iperf3 all tests

---
+++ iperf3-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#./net-test.sh -s iperf3.serv -h tango.host -o iperf3-all.log
+./net-test.sh -s iperf3.serv -h rumba.host -o iperf3-all.log
+./net-test.sh -s iperf3.serv -h mambo.host -o iperf3-all.log
+./net-test.sh -s iperf3.serv -h psarch.host -o iperf3-all.log
+./net-test.sh -s iperf3.serv -h gui3.host -o iperf3-all.log
+./net-test.sh -s iperf3.serv -h tenge.host -o iperf3-all.log
---
+++ ping-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#./net-test.sh -s ping.serv -h tango.host -o ping-all.log
+./net-test.sh -s ping.serv -h rumba.host -o ping-all.log
+./net-test.sh -s ping.serv -h mambo.host -o ping-all.log
+./net-test.sh -s ping.serv -h psarch.host -o ping-all.log
+./net-test.sh -s ping.serv -h gui3.host -o ping-all.log
+./net-test.sh -s ping.serv -h tenge.host -o ping-all.log
